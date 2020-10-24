9ee226698c0bc2cca314634698cb588e894c8b2c
z-web-developer <73073714+z-web-developer@users.noreply.github.com>
2020-10-25 02:13:33 +0330

    Updates

---
+++ precache-manifest.4955df3b334396df2ad373abda08cb9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c0e8b6263d03d449bc",
+    "url": "/z-todo/static/css/main.71dbcb3e.chunk.css"
+  },
+  {
+    "revision": "75c0e8b6263d03d449bc",
+    "url": "/z-todo/static/js/main.75c0e8b6.chunk.js"
+  },
+  {
+    "revision": "f0211dd8ec72a3bc690e",
+    "url": "/z-todo/static/js/1.f0211dd8.chunk.js"
+  },
+  {
+    "revision": "8e9d35cb4936e2efe0da",
+    "url": "/z-todo/static/js/runtime~main.8e9d35cb.js"
+  },
+  {
+    "revision": "0dd51774023bc56a9cc80629e785a1bf",
+    "url": "/z-todo/index.html"
+  }
+];
